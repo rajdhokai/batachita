13721ff579c44148e5df2db27fae3fc355a2fefb
Dhokai Raj B <chintan.dhokai@debutify.com>
2025-09-22 16:32:55 +0530

    rate limiting implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -6,8 +6,11 @@ import {
   logout,
   updateProfile,
 } from "../controllers/auth.controller.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.use(arcjetProtection);
 
 router.post("/signup", signup);
 router.post("/login", login);
