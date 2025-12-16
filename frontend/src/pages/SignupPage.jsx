e53ada3a7334590afddbac960534f5282d3b837c
codebuster <rajdhokai2000@gmail.com>
2025-12-16 10:43:54 +0530

    login & logout implemented

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -116,17 +116,18 @@ function SignupPage() {
               </div>
             </div>
             {/* FORM ILLUSTRATION - RIGHT SIDE */}
-            <div className="hidden md:1/2 md:flex items-center justify-center p-6 bg-gradient-to-bl form-slate-800/20 to-transparent">
+            <div className="hidden md:w-1/2 md:flex items-center justify-center p-6 bg-gradient-to-bl from-slate-800/20 to-transparent">
               <div>
                 <img
                   src="/signup.png"
-                  alt="Peaople using mobile devices"
-                  className="w-96 h-96 object-contain"
+                  alt="People using mobile devices"
+                  className="w-full h-auto object-contain"
                 />
                 <div className="mt-6 text-center">
                   <h3 className="text-xl font-medium text-cyan-400">
-                    Start your journey today
+                    Start Your Journey Today
                   </h3>
+
                   <div className="mt-4 flex justify-center gap-4">
                     <span className="auth-badge">Free</span>
                     <span className="auth-badge">Easy Setup</span>
